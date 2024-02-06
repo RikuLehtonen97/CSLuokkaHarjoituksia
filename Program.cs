4f650c9f29968c20c554902e30da5a2becb5a5fc
RikuLehtonen97 <47364@edu.raseko.fi>
2024-02-06 13:34:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLuokkaHarjoituksia1
+{
+    class Hooman
+    {
+        // define properties of Hooman
+        String name;
+        int Age;
+        String gender;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
