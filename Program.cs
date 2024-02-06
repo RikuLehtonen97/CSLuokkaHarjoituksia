f1c7ff1d675519ef4379ed82495845677faeffc1
RikuLehtonen97 <47364@edu.raseko.fi>
2024-02-06 14:27:57 +0200

    v1

--- Program.cs
+++ Program.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace CSLuokkaHarjoituksia1
 {
     class Hooman
     {
         // Define properties of Hooman
-        String name;
-        int Age;
-        String gender;
+        String name = "Peruna Esim";
+        int Age = 30;
+        String gender = "attackhelicopter";
+
+        // constructor with 0 argument
+        Hooman() 
+        {
+
+        }
+        // constructor with one argument
+        Hooman(string name)
+        {
+            this.name = name;
+        }
+
+        // Constructor with 2 arguments
+        Hooman(string name, int age) 
+        {
+            this.name = name;
+            this.Age = age;
+        }
+        // Constructor with 3 arguments
+        Hooman(string name, int age, string gender) 
+        {
+            this.name = name;
+            this.Age = age;
+            this.gender = gender;
+        }
     }
     internal class Program
     {
